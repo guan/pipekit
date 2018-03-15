25cfb55f980a13df500937abef3c058eae16c129
guan <kuniyasu.guan@gmail.com>
2018-03-15 16:12:11 +0900

    override activity#all

--- lib/pipekit/activity.rb
+++ lib/pipekit/activity.rb
@@ -3,5 +3,9 @@ module Pipekit
     include Repository
     SINGULAR_CLASSNAME = "activity".freeze
     PLURALIZED_CLASSNAME = "activities".freeze
+
+    def all
+      request.get("", user_id: 0)
+    end
   end
 end
